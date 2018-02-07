f072a950c2c56818b41777d0b628d15af8e4373a
misule0423 <misule0423@gmail.com>
2018-02-07 12:18:15 +0900

    Display Video Commit

---
+++ Ch2.IntroductionToOpenCV/Ch2.IntroductionToOpenCV/02-2.DisplayVideo.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <opencv2\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv){
+
+	namedWindow("Example3", WINDOW_AUTOSIZE);
+	
+	VideoCapture cap;
+	
+	cap.open("cat.avi");
+	
+	Mat frame;
+	
+	for (;;) {
+	
+		cap >> frame;
+		
+		if (frame.empty()) break; // Ran out of film
+		
+		imshow("Example3", frame);
+		
+		if (waitKey(1000) == 27) break;
+		}
+	return 0;
+
+}
